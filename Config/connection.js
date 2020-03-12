a7f8ead48a82588be8b76cb28d790f8c2de68296
Femi Fagbure <femifagbure@gmail.com>
2020-03-12 16:59:04 -0400

    connection and orm files updated

--- Config/connection.js
+++ Config/connection.js
@@ -1,1 +1,17 @@
-const mysql = require ("./node_modules/mysql");
+const mysql = require ("mysql");
+
+let connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'password',
+  database : 'burgers_db'
+});
+ 
+connection.connect();
+ 
+
+ 
+connection.end();
+
+
+module.exports = connection;
