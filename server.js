a74fe2bd163e4bac599a032905bbb0fe81d7002d
Femi Fagbure <femifagbure@gmail.com>
2020-05-05 18:25:15 -0400

    edited server file

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
+require('dotenv').config()
 var express = require('express');
 var exphbs  = require('express-handlebars');
  
 var app = express();
+const port = process.env.PORT || 3000
 app.use(express.static("public"));
  
 app.engine('handlebars', exphbs());
@@ -10,7 +12,5 @@ app.set('view engine', 'handlebars');
 app.get('/', function (req, res) {
     res.render('index');
 });
- 
-app.listen(3000);
 
 console.log("connection established!");
